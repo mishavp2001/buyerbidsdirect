6e102f984d53f8a0148d807757adde2ba32a980c
mishavp2001 <mishavp2001@yahoo.com>
2024-05-21 12:06:53 -0700

    remove todo refs

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,13 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>For sale houses:</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
     </main>
   );
 }
