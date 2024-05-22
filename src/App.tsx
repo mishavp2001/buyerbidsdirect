9adcd71e4d125e722c4c6afe74efb06afb15df6a
Michael Polyakov <mishavp2001@yahoo.com>
2024-05-21 17:23:28 -0700

    feat: add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Authenticator } from '@aws-amplify/ui-react'
 
+import '@aws-amplify/ui-react/styles.css'
 const client = generateClient<Schema>();
 
 function App() {
@@ -17,16 +19,28 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
-    <main>
-      <h1>For sale:</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-    </main>
+
+    <Authenticator>
+      {({ signOut, user }) => (
+
+        <main>
+          <h1>For sale:</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
+            ))}
+          </ul>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+
+      )}
+    </Authenticator>
   );
 }
 
