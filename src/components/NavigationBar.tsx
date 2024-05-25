79b94e7838d3f074029016c814a3fa8c8a5ff898
Michael Polyakov <mishavp2001@yahoo.com>
2024-05-24 19:15:38 -0700

    feat: removed unused

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
 
 const NavigationBar: React.FC = () => {
     const { user, authStatus, signOut } = useAuthenticator((context) => [
