3cc1fbf072af744811738a891465d44fba499a11
Michael Polyakov <mishavp2001@yahoo.com>
2024-05-26 12:06:24 -0700

    fix: route frpom sale submit

--- src/pages/SellProperty.tsx
+++ src/pages/SellProperty.tsx
@@ -2,14 +2,16 @@
 import React from 'react';
 import { Container, Paper } from '@mui/material';
 import PropertyCreateForm from '../../ui-components/PropertyCreateForm';
+import { useNavigate, useLocation } from 'react-router-dom';
  
 const SellProperty: React.FC = () => {
+  const navigate = useNavigate();
 
   return (
     <Container>
       <Paper elevation={3} sx={{ padding: 3 }}>
       <h3>Sell your property </h3>
-      <PropertyCreateForm />
+      <PropertyCreateForm onSuccess={()=>{ navigate("/", { replace: true });}}/>
       </Paper>
     </Container>
   );
