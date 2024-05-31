35d42c8f0c794197324fe923622f089e29a98d9e
Michael Polyakov <mishavp2001@yahoo.com>
2024-05-30 18:40:34 -0700

    feat: add storage

---
+++ src/components/AddImage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { uploadData } from 'aws-amplify/storage';
+
+export default function AddImage() {
+  const [file, setFile] = React.useState();
+
+  const handleChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} />
+        <button
+          onClick={() =>
+            uploadData({
+              path: `picture-submissions/${file.name}`,
+              data: file,
+          })
+        }
+      >
+        Upload
+      </button>
+    </div>
+  );
+}
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,10 +1,12 @@
 import '@aws-amplify/ui-react/styles.css'
 import MapWithItems from '../components/MapWithItems';
+import AddImage from '../components/AddImage';
 
 function homePage() {
     return (
         <main>         
         <MapWithItems />
+        <AddImage />
       </main>
     );
 }
