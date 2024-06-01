2904fdba655f6c18f1c4b77ddba0eaac3e275fba
Michael Polyakov <mishavp2001@yahoo.com>
2024-06-01 12:09:20 -0700

    feat: google maps

--- src/components/MapWithItems.tsx
+++ src/components/MapWithItems.tsx
@@ -1,27 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import ListItems from './ListItems';
 import type { Schema } from "../../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Link } from 'react-router-dom';
-import 'leaflet/dist/leaflet.css';
-import { icon } from "leaflet"
+import { APIProvider, Map, AdvancedMarker, useAdvancedMarkerRef, InfoWindow } from '@vis.gl/react-google-maps';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { StorageImage } from '@aws-amplify/ui-react-storage';
 import { NumericFormat } from 'react-number-format';
 
-const ICON = icon({
-  iconUrl: "/marker.png",
-})
 
 const client = generateClient<Schema>();
 
 
 const MapWithItems: React.FC = () => {
   const [itemsForSale, setItemsForSale] = useState<any[]>([]);
-  const [position, setPosition] = useState<[number, number] | null>(null);
+  const [position, setPosition] = useState<{ lat: number, lng: number } | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const [showMap, setShowMap] = useState<boolean>(false); // State to toggle map view
+  const [showMap, setShowMap] = useState<boolean>(true); // State to toggle map view
   const [properties, setProperties] = useState<Array<any>>([]); // Adjust the type according to your schema
   const { user } = useAuthenticator((context) => [context.user]);
 
@@ -40,18 +35,21 @@ const MapWithItems: React.FC = () => {
 
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (geoPosition) => {
-          const userPosition: [number, number] = [geoPosition.coords.latitude, geoPosition.coords.longitude];
-          setPosition(userPosition);
-        })
-    } else {
-      setError('Geolocation is not supported by this browser.');
+    if (showMap) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (geoPosition) => {
+            const userPosition = { lat: geoPosition.coords.latitude, lng: geoPosition.coords.longitude };
+            setPosition(userPosition);
+          })
+      } else {
+        setError('Geolocation is not supported by this browser.');
+      }
     }
+
     setItemsForSale(properties);
 
-  }, [properties]);
+  },[properties]);
 
 
 
@@ -66,6 +64,8 @@ const MapWithItems: React.FC = () => {
   if (!properties) {
     return <div>Loading...</div>;
   }
+  const [infowindowOpen, setInfowindowOpen] = useState(false);
+  const [markerRef, marker] = useAdvancedMarkerRef();
 
   return (
     <div className='list-items'>
@@ -73,47 +73,62 @@ const MapWithItems: React.FC = () => {
         {showMap ? 'Show List' : 'Show Map'}
       </button>
       {showMap && position ? (
-        <MapContainer center={position} zoom={13} style={{ height: '50vh', width: '100%' }}>
-          <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://www.carto.com/attributions">CARTO</a>'
-          />
-          {
-            itemsForSale.map(item => (
-              item?.position && <Marker icon={ICON} key={item.id} position={[JSON.parse(item?.position).latitude, JSON.parse(item?.position).longitude]}>
-                <p>{item?.position}</p>
-                <Popup className="prop-popup">
-                  <div style={{
-                    width: "90%",
-                    marginTop: "30px",
-                    overflow: 'scroll'
-                  }}>
-                    <p>{item?.description}</p>
-                    <strong>Price: </strong>
-                      <NumericFormat value={item?.price.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} />
-                    <br />
-                    <strong>Square Footage: </strong>
-                    <NumericFormat value={item.squareFootage.toFixed(0)} displayType={'text'} thousandSeparator={true} suffix={'sqft'} /> 
-    
-                    {item.photos.map((img: string) => {
-                      return <StorageImage alt='test' width='100%' path={img} />;
-                    })}
-                    <p>
-                      {user?.username === item.owner ? (
-                        <Link to={`/sales/${item.id}`}>
-                          Edit
-                        </Link>
-                      ) : (
-                        <Link to={`/offers/null/${item?.address}/${item.id}/${item.owner}`}>
-                          Make Offer
-                        </Link>
-                      )}
-                    </p>
-                  </div>
-                </Popup>
-              </Marker>
-            ))}
-        </MapContainer>
+
+
+        <APIProvider apiKey={'AIzaSyC1m92_wrOPapYEQu2JYP919wNartB3kDA'}>
+          <Map
+            style={{ width: "100vw", height: "100vh" }}
+            defaultZoom={14}
+            mapId={'cc9ee0ccda8ba6ba'}
+            defaultCenter={position}>
+            {
+              itemsForSale.map((item, index )=> {
+                const lat = JSON.parse(item?.position).latitude;
+                const lng = JSON.parse(item?.position).longitude;
+                console.log(item?.description,lat, lng);
+                return (  
+                <>
+                <AdvancedMarker
+                  key={index}
+                  position={{ 'lat': lat, 'lng': lng }}
+                  title={item?.description}
+                  ref={markerRef}
+                  onClick={() => setInfowindowOpen(true)}
+                >
+                  {infowindowOpen && (
+                    <InfoWindow
+                      anchor={marker}
+                      maxWidth={200}
+                      onCloseClick={() => setInfowindowOpen(false)}>
+                      <p>{item?.position}</p>
+                      <div>
+                        <p>{item?.description}</p>
+                        <strong>Price: </strong>
+                        <NumericFormat value={item?.price.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                        <br />
+                        <strong>Square Footage: </strong>
+                        <NumericFormat value={item.squareFootage.toFixed(0)} displayType={'text'} thousandSeparator={true} suffix={'sqft'} />
+
+
+                        <p>
+                          {user?.username === item.owner ? (
+                            <Link to={`/sales/${item.id}`}>
+                              Edit
+                            </Link>
+                          ) : (
+                            <Link to={`/offers/null/${item?.address}/${item.id}/${item.owner}`}>
+                              Make Offer
+                            </Link>
+                          )}
+                        </p>
+                      </div>
+                    </InfoWindow>)}
+                </AdvancedMarker>
+                </>
+              )})}
+          </Map>
+        </APIProvider>
+
       ) : (
         <ListItems properties={properties} />
       )}
